16935b79896647544b13be044f1805967717b117
Cattleya Pia <cjpia612@gmail.com>
2020-08-04 05:54:04 -0700

    created api routes skeleton, created server to connect to api middleware

---
+++ routes/api-routes.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const express = require ("express");
+const Workout = require("../models/workoutModel.js");
+
+router.get("/api/workouts", (req, res) =>{
+
+});
+router.post("/api/workouts", (req, res) =>{
+
+});
+router.put("/api/workouts:id", (req, res) =>{
+
+});
+router.get("/api/workouts/range", (req, res) =>{
+
+});
