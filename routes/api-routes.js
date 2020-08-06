546aaa2b1d9e3248c3cdd0e7a15f1c0b567b352b
Cattleya Pia <cjpia612@gmail.com>
2020-08-05 22:41:07 -0700

    added api-routes, functional, required api-route on server

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,19 +1,50 @@
-const { Router } = require("express");
-
 const router = require("express").Router();
 const mongoose = require("mongoose");
 const express = require ("express");
 const Workout = require("../models/workoutModel.js");
 
 router.get("/api/workouts", (req, res) =>{
-
+    Workout.find({})
+        .sort({ date: 1 })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 });
-router.post("/api/workouts", (req, res) =>{
-
+router.post("/api/workouts", ({ body }, res) =>{
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 });
 router.put("/api/workouts:id", (req, res) =>{
-
+    Workout.update(
+        { 
+            _id: req.params.id
+        },
+        {
+            $set: {
+                exercises: req.body
+            }
+        }
+    ).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
 });
 router.get("/api/workouts/range", (req, res) =>{
-
+    Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
 });
+
+module.exports = router;
